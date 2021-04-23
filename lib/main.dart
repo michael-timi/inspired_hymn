67fd50bbaf8b41cd3e3424460d90aa6eeb4c8d7c
michtimiael <michtimiael@gmail.com>
2021-04-23 12:48:33 +0100

    This is first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:inspired_gospel_hymn/view/menu_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,17 +10,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Inspired Gospel Hymn',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: Color(0xff392850),
-          accentColor: Color(0xFFFEF9EB),
-          backgroundColor: Color(0xff392850),
-          textTheme: TextTheme(
-            bodyText1: GoogleFonts.openSans(),
-            bodyText2: GoogleFonts.openSans(),
-          ),
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ));
+      title: 'Inspired Gospel Hymn',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xff392850),
+        accentColor: Color(0xFFFEF9EB),
+        backgroundColor: Color(0xff392850),
+        textTheme: TextTheme(
+          bodyText1: GoogleFonts.openSans(),
+          bodyText2: GoogleFonts.openSans(),
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MenuPage(),
+    );
   }
 }
